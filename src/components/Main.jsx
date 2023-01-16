fa8aba841a997f2b6c371cf999484707cdaa4ded
zgronska <zgronska@gmail.com>
2023-01-16 21:28:41 +0100

    Initial commit

---
+++ src/components/Main.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+export default function Main() {
+  return <h1>Main goes here</h1>;
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+export default function Navbar() {
+  return <h1>Navbar goes here</h1>;
+}

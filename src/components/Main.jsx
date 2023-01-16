32178d6cdbb8d5fa919f5f811043d13da365745d
zgronska <zgronska@gmail.com>
2023-01-16 22:32:52 +0100

    Finished most styling

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,18 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import BgImg from "../images/reactjs-icon-2.svg";
 
 export default function Main() {
-  return <h1>Main goes here</h1>;
+  return (
+    <main>
+      <h1 className="main--title">Fun facts about React</h1>
+      <ul className="main--facts">
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100K stars on GitHub</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+      {/* <img src={BgImg} alt="Large React logo" className="main--bg-img" /> */}
+    </main>
+  );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import Logo from "../images/reactjs-icon.svg";
 
 export default function Navbar() {
-  return <h1>Navbar goes here</h1>;
+  return (
+    <nav>
+      <img src={Logo} alt="React logo" className="nav--image" />
+      <h2 className="nav--title">ReactFacts</h2>
+      <h3 className="nav--subtitle">React Course - Project 1</h3>
+    </nav>
+  );
 }
